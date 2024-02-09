f094c6231a90f482d48d52b039487b5aed745ced
Tony Zatelli <151331+azatelli@users.noreply.github.com>
2024-02-09 10:40:50 -0500

    Change version target to 16.6 for Github action support.

--- slinky/ContentView.swift
+++ slinky/ContentView.swift
@@ -6,11 +6,9 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    let items: [Item] = []
 
     var body: some View {
         NavigationSplitView {
@@ -40,22 +38,15 @@ struct ContentView: View {
     }
 
     private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
+        
     }
 
     private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
+        
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+        
 }
--- slinky/Item.swift
+++ slinky/Item.swift
@@ -6,13 +6,13 @@
 //
 
 import Foundation
-import SwiftData
 
-@Model
-final class Item {
+final class Item: Identifiable {
+    var id: UUID
     var timestamp: Date
     
     init(timestamp: Date) {
+        self.id = UUID()
         self.timestamp = timestamp
     }
 }
