4b9899ee04ee40c65eea3945759b3e6806e617e9
Tony Zatelli <151331+azatelli@users.noreply.github.com>
2024-02-09 10:03:05 -0500

    Initial Commit

---
+++ slinky/slinkyApp.swift
@@ -0,0 +1,32 @@
+//
+//  slinkyApp.swift
+//  slinky
+//
+//  Created by Tony Zatelli on 2/9/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct slinkyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
